b01da433321f41e5bda367d6098573bfc28dcc79
Blessonjoshua1234 <113964157+Blessonjoshua1234@users.noreply.github.com>
2022-11-22 11:25:52 +0530

    add

---
+++ finishing deliverables/project code/sprin4/app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/index')
+def index():
+    return render_template("view.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict(final)[0]
+
+    if prediction==1:
+        return render_template('view.html',pred='This website is safe.'.format(prediction))
+    else:
+        return render_template('view.html',pred='This website is not safe.'.format(prediction))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
